4df966648bdd34f1cbb4a3712fe52d733c921178
Hugo <43464879+ziberty@users.noreply.github.com>
2022-06-02 15:13:50 +0200

    base app

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -23,6 +23,9 @@ if (app.Environment.IsDevelopment())
     //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
